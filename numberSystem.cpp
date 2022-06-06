5d02183679c6ee472db2aa58350ec3d43945421a
Nikita <nikitajain267@gmail.com>
2022-06-06 14:50:12 +0530

    Decimal in all Number Systems

---
+++ numberSystem.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+// Representatio of 15 in all Number Systems
+
+int main() {
+    int number1 = 15;           //Decimal
+    int number2 = 0X0F;         //HexaDecimal
+    int number3 = 017;          //Octal
+    int number4 = 0b00001111;    //Binary
+
+    std::cout << "Number1 = " << number1 << std::endl;
+    std::cout << "Number1 = " << number1 << std::endl;
+    std::cout << "Number1 = " << number1 << std::endl;
+    std::cout << "Number1 = " << number1 << std::endl;
+
+    return EXIT_SUCCESS;
+}
